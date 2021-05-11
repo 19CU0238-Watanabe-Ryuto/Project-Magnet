350c561b48d2df206c2688d7c71fac22f32017c1
Ryuto Watanbe <19cu0238@jec.ac.jp>
2021-05-11 16:33:06 +0900

    カメラのクラスにロックオン機能の追加 レティクルUIの追加

--- Source/Teleport/TPSCameraComponent.cpp
+++ Source/Teleport/TPSCameraComponent.cpp
@@ -1,22 +1,27 @@
 // TPS視点でのカメラの制御・管理を行うクラス
 //
 // 2021/05/10 渡邊龍音 作成・レイを画面中央に飛ばし当たり判定をとる
+// 2021/05/11 渡邊龍音 カメラのロックオンを作成
 
 #include "TPSCameraComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "DrawDebugHelpers.h"
 #include "Engine/World.h"
 
 // Sets default values for this component's properties
 UTPSCameraComponent::UTPSCameraComponent()
-	: cameraComponent(nullptr)
-	, enableLineTrace(true)
-	, isDrawDebugLine(true)
-	, drawDebugLineTime(0.5f)
-	, rayLength(5000.0f)
-	, rayOffset(FVector::ZeroVector)
-	, isHit(false)
+	: m_IsHit(false)
+	, m_IsLockOn(false)
+	, m_LockOnActor(nullptr)
+	, m_CameraComponent(nullptr)
+	, m_PlayerCharacter(nullptr)
+	, m_EnableLineTrace(true)
+	, m_IsDrawDebugLine(true)
+	, m_DrawDebugLineTime(0.5f)
+	, m_RayLength(5000.0f)
+	, m_RayOffset(FVector::ZeroVector)
 {
 	PrimaryComponentTick.bCanEverTick = true;
 }
@@ -35,38 +40,92 @@ void UTPSCameraComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ライントレースをしない場合、関数を終了
-	if (!enableLineTrace)
+	if (!m_EnableLineTrace)
 	{
 		return;
 	}
 
 	// カメラコンポーネント未設定の場合、ログを表示し関数を終了
-	if (cameraComponent == nullptr)
+	if (m_CameraComponent == nullptr)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("[TPSCameraComponent] camera is null."));
 		return;
 	}
 
 	// レイ開始位置
-	FVector start = cameraComponent->GetComponentLocation();
+	FVector start = m_CameraComponent->GetComponentLocation();
 
 	// レイ終了位置
-	FVector forwardVec = UKismetMathLibrary::GetForwardVector(cameraComponent->GetComponentRotation()) * rayLength;
-	FVector end = start + forwardVec + rayOffset;
+	FVector forwardVec = UKismetMathLibrary::GetForwardVector(m_CameraComponent->GetComponentRotation()) * m_RayLength;
+	FVector end = start + forwardVec + m_RayOffset;
 
 	// デバッグ確認用のラインを描画
-	if (isDrawDebugLine)
+	if (m_IsDrawDebugLine)
 	{
-		DrawDebugLine(GetWorld(), start, end, FColor::Green, false, drawDebugLineTime);
+		DrawDebugLine(GetWorld(), start, end, FColor::Green, false, m_DrawDebugLineTime);
 	}
 
-	isHit = GetWorld()->LineTraceSingleByChannel(hitResult, start, end, ECollisionChannel::ECC_Visibility);
+	// レイを飛ばし当たり判定を取る
+	m_IsHit = GetWorld()->LineTraceSingleByChannel(m_HitResult, start, end, ECollisionChannel::ECC_Visibility);
+
+	// ロックオン処理
+	LockOn();
+}
+
+
+// ロックオン処理関数
+void UTPSCameraComponent::LockOn()
+{
+	// ロックオン状態でなければ処理を終了
+	if (!m_IsLockOn)
+	{
+		UE_LOG(LogTemp, Verbose, TEXT("[TPSCameraComponent] Is not Lock-On."));
+		return;
+	}
+
+	// ロックオン対象が有効でなければ処理を終了
+	if (m_LockOnActor == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[TPSCameraComponent] LockOnActor is nullptr."));
+		return;
+	}
+
+	FVector targetLocation = m_LockOnActor->GetActorLocation();
+	FRotator rot = UKismetMathLibrary::FindLookAtRotation(m_CameraComponent->GetComponentLocation(), targetLocation);
+
+	m_PlayerCharacter->GetController()->SetControlRotation(rot);
 }
 
 
 // 初期化関数
-void UTPSCameraComponent::Init(UCameraComponent* _camera)
+void UTPSCameraComponent::Init(UCameraComponent* _camera, ACharacter* _character)
 {
-	cameraComponent = _camera;
+	m_CameraComponent = _camera;
+	m_PlayerCharacter = _character;
 }
 
+
+// ロックオン状態にする関数
+void UTPSCameraComponent::SwitchLockOn()
+{
+	// ロックオン状態のスイッチ
+	m_IsLockOn = !m_IsLockOn;
+
+	if (m_IsLockOn)
+	{
+		m_LockOnActor = m_HitResult.GetActor();
+
+		if (m_LockOnActor != nullptr)
+		{
+			UE_LOG(LogTemp, Verbose, TEXT("[TPSCameraComponent] LockOnActor name : %s ."), *m_LockOnActor->GetName());
+		}
+		else
+		{
+			UE_LOG(LogTemp, Verbose, TEXT("[TPSCameraComponent] LockOnActor is nullptr."));
+		}
+	}
+	else
+	{
+		m_LockOnActor = nullptr;
+	}
+}
--- Source/Teleport/TPSCameraComponent.h
+++ Source/Teleport/TPSCameraComponent.h
@@ -1,6 +1,7 @@
 // TPS視点でのカメラの制御・管理を行うクラス
 //
 // 2021/05/10 渡邊龍音 作成・レイを画面中央に飛ばし当たり判定をとる
+// 2021/05/11 渡邊龍音 カメラのロックオンを作成
 
 #pragma once
 
@@ -11,12 +12,12 @@
 
 class UCameraComponent;
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class TELEPORT_API UTPSCameraComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UTPSCameraComponent();
 
@@ -24,46 +25,75 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+private:
+	// レイがほかのActorにあたっているか
+	bool m_IsHit;
+
+	// レイが当たった結果
+	FHitResult m_HitResult;
+
+	// ロックオンしているか
+	bool m_IsLockOn;
+
+	// ロックオン時のActor
+	AActor* m_LockOnActor;
+
+// private変数読み取り用関数
+public:
+	// hitResult取得用
+	UFUNCTION(BlueprintPure)
+		FHitResult GetHitResult() { return m_HitResult; }
+
+	// isHit取得用
+	UFUNCTION(BlueprintPure)
+		bool GetIsHit() { return m_IsHit; }
+
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	// カメラコンポーネント
 	UPROPERTY(EditAnyWhere)
-		UCameraComponent* cameraComponent;
+		UCameraComponent* m_CameraComponent;
+
+	// プレイヤーコンポーネント
+	UPROPERTY(EditAnyWhere)
+		ACharacter* m_PlayerCharacter;
 
 	// ライントレースを有効にするか（レイを飛ばすか）
 	UPROPERTY(EditAnyWhere)
-		bool enableLineTrace;
+		bool m_EnableLineTrace;
 
 	// デバッグ用にレイを描画するか
 	UPROPERTY(EditAnyWhere, Category = "Debug")
-		bool isDrawDebugLine;
+		bool m_IsDrawDebugLine;
 
 	// デバッグ用レイの表示時間
 	UPROPERTY(EditAnyWhere, Category = "Debug")
-		float drawDebugLineTime;
+		float m_DrawDebugLineTime;
 
 	// レイの距離
 	UPROPERTY(EditAnyWhere)
-		float rayLength;
+		float m_RayLength;
 
 	// レイのオフセット位置
 	UPROPERTY(EditAnyWhere)
-		FVector rayOffset;
+		FVector m_RayOffset;
 
-	// レイがほかのActorにあたっているか
-	UPROPERTY(VisibleAnyWhere)
-		bool isHit;
-
-	// レイが当たった結果
-	UPROPERTY(VisibleAnyWhere)
-		FHitResult hitResult;
+private:
+	// ロックオン処理関数
+	void LockOn();
 
 public:
 	// 初期化関数
 	// 
-	// 第一引数 カメラコンポーネント
+	// 第一引数 対象になるカメラコンポーネント
 	UFUNCTION(BlueprintCallable)
-		void Init(UCameraComponent* _camera);
+		void Init(UCameraComponent* _camera, ACharacter* _character);
+
+	// ロックオン状態にする関数
+	//
+	// 引数なし
+	UFUNCTION(BlueprintCallable)
+		void SwitchLockOn();
 };
